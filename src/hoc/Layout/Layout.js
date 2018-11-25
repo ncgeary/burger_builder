23a457fcbff885f3b10fd13a926220b46a33d9dd
Nicholas Geary <ncgeary@buffalo.edu>
2018-11-25 14:31:18 -0500

    11/25/2018

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import Aux from '../Aux/Aux'
+import Aux from '../Aux1/Aux1'
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
